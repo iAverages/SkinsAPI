24929a32697ababb71b6ca9cf84f9593cb830040
iAvearges <31937542+iAverages@users.noreply.github.com>
2021-08-25 00:55:46 +0100

    Fixed typo + schema

--- src/database/schema/profile.js
+++ src/database/schema/profile.js
@@ -1,23 +1,29 @@
 const { Schema, model } = require("mongoose");
 
+// TODO: Implement a no user cache.
+// Basically a mongo collection which stores users which
+// Have been requested but are not a user on mc.
+// Stops sending repeat requests for users that dont exist.
+// Ofc with a timeout to recheck
+
 const schema = new Schema({
-    time: { type: Number, required: true }, // Time last updated
-    _id: { type: String, required: true }, // Players UUID
-    name: { type: String, required: true },
-    assests: {
+    time: { type: Number, default: Date.now() }, // Time last updated
+    _id: { type: String, required: true, alias: "uuid" }, // Players UUID
+    name: { type: String, required: true, index: true, unique: true },
+    assets: {
         skin: {
             url: { type: String, required: true },
             base64: { type: String, required: true },
             slim: { type: Boolean, default: false },
         },
         cape: {
-            url: { type: String, required: true },
-            base64: { type: String, required: true },
+            url: { type: String },
+            base64: { type: String },
         },
     },
 });
 
-schema.method("transform", function () {
+schema.method("toJSON", function () {
     const obj = this.toObject();
     // Replace _id with uuid
     obj.uuid = obj._id;
