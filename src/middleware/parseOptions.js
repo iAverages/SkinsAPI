c610b951f3460db431b09b2942519bfd052c0f45
iAvearges <31937542+iAverages@users.noreply.github.com>
2021-08-25 00:54:28 +0100

    Crated parseOptions middleware

---
+++ src/middleware/parseOptions.js
@@ -0,0 +1,20 @@
+module.exports = (req, _, next) => {
+    const q = req.query;
+
+    // Basically what this does is, ensure both width and height are set.
+    // If both are specified in the query, it'll use those (Math.min to 1000)
+    // If only one is set, both are set to the same value (Whatever one is set)
+    // If neither are set, defaults to 300
+    const width = Math.min(1000, check(q?.width) ? +q.width : check(q?.height) ? +q.height : 300);
+    const height = Math.min(1000, check(q?.height) ? +q.height : width);
+
+    req.options = {
+        base64: q.base64?.toLowerCase() === "true",
+        overlay: q.overlay ? q.overlay.toLowerCase() === "true" : true, // overlay defaults to true
+        width: width,
+        height: height,
+    };
+    next();
+};
+
+const check = (n) => !(n === null || +n <= 0 || isNaN(+n));
