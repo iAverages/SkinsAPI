7e6534c5f3a5100b31d4cf0c04835940af293993
iAvearges <31937542+iAverages@users.noreply.github.com>
2021-08-25 17:04:46 +0100

    Wrapped route in catchAsync

--- src/routes/v1/head.js
+++ src/routes/v1/head.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const catchAsync = require("../../helpers/catchAsync");
 const controller = require("../../controller/v1/head");
 
 // Gets the head of the specified user
-router.get("/:name", controller.getHead);
+router.get("/:name", catchAsync(controller.getHead));
 
 module.exports = router;
