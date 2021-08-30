82c6999d291ad6958baa7cbcd2e9eede299d64e3
dan <31937542+iAverages@users.noreply.github.com>
2021-08-31 00:09:41 +0100

    Fixed issue with env vars not loading.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: __dirname + "../.env" });
+require("dotenv").config({ path: __dirname + "/../.env" });
 const express = require("express");
 const middleware = require("./middleware");
 const log = require("./helpers/logger");
